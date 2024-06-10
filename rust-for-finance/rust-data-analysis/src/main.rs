deab616f4bd14965b2b68c6dbded4bc5e1bf78ba
mindeleven <mindeleven@posteo.de>
2024-06-10 11:34:49 +0200

    Using reqwest to get the Bitcoin price from Coinbase

--- rust-for-finance/rust-data-analysis/src/main.rs
+++ rust-for-finance/rust-data-analysis/src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+/// https://blog.logrocket.com/making-http-requests-rust-reqwest/
+
+// tokio let's us use "async" on our main function
+#[tokio::main]
+async fn main() {
+    // chaining .await will yield our query result
+    let response = reqwest::get("https://api.coinbase.com/v2/prices/spot?currency=USD")
+        .await
+        // each response is wrapped in a `Result` type
+        // we'll unwrap here for simplicity
+        .unwrap()
+        .text()
+        .await;
+
+    println!("{:?}", response);
+
 }
